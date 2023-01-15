2c9fe91c9fae6be790aeaade7b5f29679db0fdea
Olmos Gustavo <grolmos159@gmail.com>
2023-01-15 01:54:26 +0100

    Primer Commit

---
+++ js/function.js
@@ -0,0 +1,6 @@
+// eventos de java. evento boton.
+
+document.getElementById('boton').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
+}
